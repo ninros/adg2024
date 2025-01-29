644213f61cca302f879018fa2c1977fb49eefe12
Nina <129658564+ninros@users.noreply.github.com>
2025-01-29 18:06:22 +0100

    Ad8

---
+++ zadania/adg8.py
@@ -0,0 +1,24 @@
+#ad8
+import numpy as np
+
+def cal_stats(layer):
+    areas = []
+    lengths = []
+    for feature in layer.getFeatures():
+        geom = feature.geometry()
+        areas.append(geom.area())
+        lengths.append(geom.length())
+    def stats(values):
+        return{
+        "min": np.min(values),
+        "max": np.max(values),
+        "mean": np.mean(values),
+        "median": np.median(values),
+        "std_dev": np.std(values)
+        }
+    return {"area": stats(areas), "length": stats(lengths)}
+
+vector_layer = QgsVectorLayer("powiaty.gpkg", "powiaty", "ogr")
+stats = cal_stats(vector_layer)
+print("Statystyki dla powierzchni:", stats["area"])
+print("Statystyki dla długości:", stats["length"])
